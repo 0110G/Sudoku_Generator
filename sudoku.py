e431d32f7e52a8b187ff9b6eff01eac4b18e6373
0110G <39427377+0110G@users.noreply.github.com>
2018-06-20 19:28:12 +0530

    Improvised Code
    Added ability to solve a sudoku as well. New and more efficient backtracking algorithm used to solve and generate a sudoku. Major bugs fixed in the function sudo_chk().

--- sudoku.py
+++ sudoku.py
@@ -1,149 +1,165 @@
+# ************************************************* SUDOKU SOLVER AND GENERATOR ************************************************************
+# Instructions : To solve sudoku, initialize the matrix de to the required sudoku. To generate a sudoku, initialize the matrix de to all 0.
+# AUTHOR : Bhavya Saraf
 import random
-
-count = 0		
-i = 0						  #i is the row coordinate
-j = 0						  #j is the column coordinate 
-de = [[0,0,0,0,0,0,0,0,0],    #de is template sudoku 
-	  [0,0,0,0,0,0,0,0,0],
-	  [0,0,0,0,0,0,0,0,0],
-	  [0,0,0,0,0,0,0,0,0],
-	  [0,0,0,0,0,0,0,0,0],
-	  [0,0,0,0,0,0,0,0,0],
-	  [0,0,0,0,0,0,0,0,0],
-	  [0,0,0,0,0,0,0,0,0],
-	  [0,0,0,0,0,0,0,0,0]]
 
 #checks if the current sudoku is valid or not
 def sudo_chk (x):
-	
-	#checks across rows 
-	for i in x:
-		for j in i:
-			s = set()
-			if j in s and j != 0 : return 0
-			s.add(j)
-	
-	#checks across columns
-	for i in range (9):
-		for j in x:
-			s = set()
-			if j[i] in s and j[i]!= 0 : return 0
-			s.add(j[i])
+    
+    #checks across rows 
+    for i in x:
+        for j in i:
+            s = set()
+            if j in s and j != 0 : return 0
+            s.add(j)
+            if j == 0:
+                continue
+    
+    #checks across columns
+    for i in range (9):
+        s = set()
+        for j in x:
+            if j[i] in s and j[i]!= 0 : return 0
+            s.add(j[i])
+            if j[i] == 0:
+                continue
 
-	#checks across 3*3 grids
-	for i in range (1,8,3):
-		for j in range (1,8,3):
-			s = set()
-			temp_row = i+2
-			temp_col = j+2
-			for u in range (i-1,temp_row):
-				for v in range (j-1,temp_col):
-					if x[u][v] in s and x[u][v]!=0 : return 0
-					s.add(x[u][v])
+    #checks across 3*3 grids
+    for i in range (1,8,3):
+        for j in range (1,8,3):
+            s = set()
+            temp_row = i+2
+            temp_col = j+2
+            for u in range (i-1,temp_row):
+                for v in range (j-1,temp_col):
+                    if x[u][v] in s and x[u][v]!=0 : return 0
+                    s.add(x[u][v])
+                    if x[u][v] == 0:
+                        continue
 
-	return 1
+    return 1
 
 #returns a set consisting of possible values at coordinates i,j
 def sudo_pos (x,i,j):
 
-	s = set([1,2,3,4,5,6,7,8,9])
-	
+    s = set([1,2,3,4,5,6,7,8,9])
+    
 
-	for u in range (9):
+    for u in range (9):
 
-		if u!=i:
-			if x[u][j]!=0:
-				if x[u][j] in s:
-					s.remove(x[u][j])
+        if u!=i:
+            if x[u][j]!=0:
+                if x[u][j] in s:
+                    s.remove(x[u][j])
 
-		if u!=j:
-			if x[i][u]!=0:
-				if x[i][u] in s:
-					s.remove(x[i][u])
+        if u!=j:
+            if x[i][u]!=0:
+                if x[i][u] in s:
+                    s.remove(x[i][u])
 
-	temp_row = ((i//3)*3)+3
-	temp_col = ((j//3)*3)+3
-	
-	for u in range ( ( (i//3)*3 ),temp_row):
-		for v in range ( ( (j//3)*3),temp_col):
-			if u!=i and v!=j:
-				if x[u][v]!=0:
-					if x[u][v] in s:
-						s.remove(x[u][v])
+    temp_row = ((i//3)*3)+3
+    temp_col = ((j//3)*3)+3
+    
+    for u in range ( ( (i//3)*3 ),temp_row):
+        for v in range ( ( (j//3)*3),temp_col):
+            if u!=i and v!=j:
+                if x[u][v]!=0:
+                    if x[u][v] in s:
+                        s.remove(x[u][v])
 
+    return s 
 
-	if len(s) == 0 : return 0
-	return s 
+#checks which position of the given sudoku are assigned values.
+def sudo_chk_zero():
+    sa=set()
+    global de 
+    for u in range(9) :
+        for v in range (9):
+            if de[u][v] != 0 :
+                sa.add((u,v))
+    return sa
 
-sudo_iter = 0
+#advances the sudoku indexes by one unit columnwise
+def front_track():
+    global i,j
+    if j==8:
+        i = i+1
+        j = 0
+    else:
+        j = j+1
 
-#generates the sudoku recursively. In order to stay within recursive limits, variable sudo_iter is introduced.  
+#generates the sudoku recursively and backtracking. In order to stay within recursive limits, variable sudo_iter is introduced.  
 def sudo_gen ():
-	global de
-	global i
-	global j
-	global sudo_iter
-	sudo_iter = sudo_iter + 1
-	if (sudo_iter>900):
-		return 0
-	else :
+    global de,non_zero_pos
+    global i
+    global j
 
-		if i*j == 64 and len(sudo_pos(de,8,8))==1 :
-			return 1
+    if i*j == 64  :
+        return 1
 
-		else:
-			if sudo_pos(de,i,j) == 0:
-				i = 0
-				j = 0
-				sudo_gen()
+    if (i,j) not in non_zero_pos :
+        
 
-			else:
-				de[i][j] = random.sample(sudo_pos(de,i,j),1)[0]
-				if j==8:
-					i = i+1
-					j = 0
-				else:
-					j = j+1
+        sample_set = sudo_pos(de,i,j)
+        while (len(sample_set)!=0) :
+            de[i][j] = random.sample(sample_set,1)[0]
+            rem_x = i
+            rem_y = j
+            sample_set.remove(de[i][j])
+            front_track()
+            if sudo_gen() == 1 :
+                return 1
+            i = rem_x
+            j = rem_y
+            de[rem_x][rem_y] = 0
 
-				sudo_gen()
+        return 0
+
+    else :
+        front_track()
+        if sudo_gen() == 1 :
+            return 1 
 
 #print sudoku generated
 def sudo_prt(x):
-	for i in range(9):
-		for j in range(9):
-			if (j+1)%3 == 0:
-				print(x[i][j],end='   ')
-			else:
-				print(x[i][j],end=' ')
-		if (i+1)%3 == 0:
-			print()
-			print()
-		else:
-			print()
+    for i in range(9):
+        for j in range(9):
+            if (j+1)%3 == 0:
+                print(x[i][j],end='   ')
+            else:
+                print(x[i][j],end=' ')
+        if (i+1)%3 == 0:
+            print()
+            print()
+        else:
+            print()
+
+#***************************INITIALIZE***********************************    
+i = 0
+j = 0
+de = [[0,0,0,0,0,1,2,3,0],
+      [1,2,3,0,0,8,0,4,0],
+      [8,0,4,0,0,7,6,5,0],
+      [7,6,5,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,1,2,3],
+      [0,1,2,3,0,0,8,0,4],
+      [0,8,0,4,0,0,7,6,5],
+      [0,7,6,5,0,0,0,0,0]]
+
+non_zero_pos = sudo_chk_zero()
+if sudo_chk(de) == 0:
+    print("NOT SOLVABLE")
+else :
+    
+    if sudo_gen() == 0:
+        print("NOT SOLVABLE")
+    else :
+        de[8][8] = random.sample(sudo_pos(de,8,8),1)[0]
+        sudo_prt(de)
+        print(sudo_chk(de))
 
 
-while (len(sudo_pos(de,8,8)) != 1):
-	count = count + 1
-	sudo_iter = 0
-	i = 0
-	j = 0
-	de = [[0,0,0,0,0,0,0,0,0],
-	  	  [0,0,0,0,0,0,0,0,0],
-	  	  [0,0,0,0,0,0,0,0,0],
-	      [0,0,0,0,0,0,0,0,0],
-	      [0,0,0,0,0,0,0,0,0],
-	      [0,0,0,0,0,0,0,0,0],
-	      [0,0,0,0,0,0,0,0,0],
-	      [0,0,0,0,0,0,0,0,0],
-	      [0,0,0,0,0,0,0,0,0]]
-	sudo_gen()
-	
-de[8][8] = random.sample(sudo_pos(de,8,8),1)[0]
+                
 
-sudo_prt(de)	
-print(sudo_chk(de))
-
-
-				
-				
 
